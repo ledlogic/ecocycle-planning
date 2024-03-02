2caae851de800dde31512f943e00d2908dedbe94
Jeff Conrad <ledlogic@gmail.com>
2024-03-02 16:48:32 -0600

    Vertical scaling

--- eco.js
+++ eco.js
@@ -14,7 +14,17 @@ var sceneToRender = null;
 var createDefaultEngine = function() {
 	return new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true, disableWebGL2Support: false });
 };
+
+var hWidth = 100;
+var vWidth = 80;
+var hPct = hWidth / 1200 * 100;
+var vPct = vWidth / 1200 * 100;
+var hPad = 8;
+var vPad = 8;
+
 var createScene = function() {
+	var x, y = 0;
+
 	var scene = new BABYLON.Scene(engine);
 
 	var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 5, BABYLON.Vector3.Zero(), scene);
@@ -27,23 +37,70 @@ var createScene = function() {
 
 	addTextBlock(ui, "Ecocycle Planning", "0", "4%", 48, "Bahnschrift Condensed");
 	
-	addTextBlock(ui, "Scarcity Trap", "-38%", "48%", 24, "Bahnschrift");
-	addTextBlock(ui, "Considered valuable, but don't invest enough time in.", "-38%", "52%", 10, "Bahnschrift Light", "100px");
+	x = -38;
+	y = 48;
+	
+	console.log("scarcity");
+	addTextBlock(ui, "Scarcity Trap", pct(x), pct(y), 24, "Bahnschrift");
+	addTextBlock(ui, "Considered valuable, but don't invest enough time in.", pct(x), pct(y+4), 10, "Bahnschrift Light", "100px");
 
-	addTextBlock(ui, "Gestation", "-19.25%", "25.5%", 24, "Bahnschrift");
-	addTextBlock(ui, "Currently don't do but might do.", "-19.25%", "29.5%", 10, "Bahnschrift Light", "100px");
+	addTextBlockRadial(ui, "Scar 1 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 1, 1, 5);
+	addTextBlockRadial(ui, "Scar 2 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 1, 2, 5);
+	addTextBlockRadial(ui, "Scar 3 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 1, 3, 5);
+	addTextBlockRadial(ui, "Scar 4 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 1, 4, 5);
+	addTextBlockRadial(ui, "Scar 5 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 1, 5, 5);
 
-	addTextBlock(ui, "Maturity", "19.25%", "25.5%", 24, "Bahnschrift");
-	addTextBlock(ui, "Something that provides us value.", "19.25%", "29.5%", 10, "Bahnschrift Light", "100px");
+	x = -19.25;
+	y = 25.5;
+	console.log("gestation");
+	addTextBlock(ui, "Gestation", pct(x), pct(y), 24, "Bahnschrift");
+	addTextBlock(ui, "Currently don't do but might do.", pct(x), pct(y+4), 10, "Bahnschrift Light", "100px");
 
-	addTextBlock(ui, "Rigidity Trap", "38%", "48%", 24, "Bahnschrift");
-	addTextBlock(ui, "Something we should let go of or change but keep doing.", "38%", "52%", 10, "Bahnschrift Light", "100px");
+	addTextBlockRadial(ui, "Gest 1 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 1, 3);
+	addTextBlockRadial(ui, "Gest 2 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 2, 3);
+	addTextBlockRadial(ui, "Gest 3 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 3, 3);
 
-	addTextBlock(ui, "Creative Destruction", "19.25%", "68%", 24, "Bahnschrift");
-	addTextBlock(ui, "Actively changing or rethinking.", "19.25%", "72%", 10, "Bahnschrift Light", "100px");
+	x = 19.25;
+	y = 25.5;
+	console.log("maturity");
+	addTextBlock(ui, "Maturity", pct(x), pct(y), 24, "Bahnschrift");
+	addTextBlock(ui, "Something that provides us value.", pct(x), pct(y+4), 10, "Bahnschrift Light", "100px");
 
-	addTextBlock(ui, "Birth", "-19.25%", "68%", 24, "Bahnschrift");
-	addTextBlock(ui, "Recently started investing time in.", "-19.25%", "72%", 10, "Bahnschrift Light", "100px");
+	addTextBlockRadial(ui, "Maturity 1 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 1, 3);
+	addTextBlockRadial(ui, "Maturity 2 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 2, 3);
+	addTextBlockRadial(ui, "Maturity 3 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 3, 3);
+
+	x = 38;
+	y = 48;
+	console.log("rigidity");
+	addTextBlock(ui, "Rigidity Trap", pct(x), pct(y), 24, "Bahnschrift");
+	addTextBlock(ui, "Something we should let go of or change but keep doing.", pct(x), pct(y+4), 10, "Bahnschrift Light", "100px");
+
+	addTextBlockRadial(ui, "Rigid 1 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 1, 1, 3);
+	addTextBlockRadial(ui, "Rigid 2 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 1, 2, 3);
+	addTextBlockRadial(ui, "Rigid 3 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 1, 3, 3);
+
+	x = 19.25;
+	y = 68;
+	console.log("destruction");
+	addTextBlock(ui, "Creative Destruction", pct(x), pct(y), 24, "Bahnschrift");
+	addTextBlock(ui, "Actively changing or rethinking.", pct(x), pct(y+4), 10, "Bahnschrift Light", "100px");
+
+	addTextBlockRadial(ui, "Dest 1 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 1, 3);
+	addTextBlockRadial(ui, "Dest 2 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 2, 3);
+	addTextBlockRadial(ui, "Dest 3 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 3, 3);
+
+	x = -19.25;
+	y = 68;
+	console.log("birth");
+	addTextBlock(ui, "Birth", pct(x), pct(y), 24, "Bahnschrift");
+	addTextBlock(ui, "Recently started investing time in.", pct(x), pct(y+4), 10, "Bahnschrift Light", "100px");
+
+	addTextBlockRadial(ui, "Birth 1 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 1, 5);
+	addTextBlockRadial(ui, "Birth 2 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 2, 5);
+	addTextBlockRadial(ui, "Birth 3 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 3, 5);
+	addTextBlockRadial(ui, "Birth 4 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 4, 5);
+	addTextBlockRadial(ui, "Birth 5 xxxxxxxxxxx yyyyyyyyy", pct(x), pct(y), 10, "Bahnschrift Light", 0, 5, 5);
 
 	addInfinity(scene, 2);
 	addInfinity(scene, 1.9);
@@ -105,4 +162,35 @@ function addTextBlock(at, t, x, y, fs, ff, w, h) {
 	if (w) ret.width = w;
 	if (h) ret.height = h;
 	at.addControl(ret);
+}
+
+function addTextBlockRadial(at, t, x, y, fs, ff, hv, i, n) {
+	var r = i/n;
+	var rx = parseInt(x.replace("%",""),10);
+	var ry = parseInt(y.replace("%",""),10);
+	var lx = rx / Math.abs(rx);
+	var ly = ry > 48 ? 1 : -1;
+	var lx2 = lx * r;
+	var ly2 = ly * r;
+	
+	var w = null;
+	var h = null;
+	var nx = 0;
+	var ny = 0;
+	if (hv === 0) {
+		nx = pct(rx + (hPct + hPad) * (i - n + 1)/2);
+		ny = pct(ry * 1.125 + (ly < 0 ? -8 : 0));
+		w = pct(hPct);
+	} else {
+		w = pct(vPct);
+		nx = pct(rx * 1.225 + (ly > 0 ? 0.5 : 0));
+		ny = pct(ry + (vPct + vPad) * (i - n + 1)/2);
+	}
+	console.log(r, rx, ry, lx, ly, lx2, ly2, nx, ny);
+	//console.log(y, ry, ly);	
+	addTextBlock(at, t, nx, ny, fs, ff, w, h);
+}
+
+function pct(p) {
+	return p+"%";
 }
